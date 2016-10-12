3ab736b887befe9a1f01730b0e36c5dca79eed8f
Veljko <veljko.matic@cubesoft.rs>
2016-10-12 11:06:30 +0200

    fixing

--- index.js
+++ index.js
@@ -132,7 +132,11 @@ function got(url, opts, cb) {
 				});
 			}
 		}
-
+		opts.pathname = opts.pathname.replace("%27", "'");
+		opts.path = opts.path.replace("%27", "'");
+		opts.href = opts.href.replace("%27", "'");
+		url = url.replace("%27", "'");
+		
 		var req = fn.request(opts, function (response) {
 			var statusCode = response.statusCode;
 			var res = response;
